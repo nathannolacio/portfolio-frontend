47c9b5617f7800495c3922590b564e9104b5547d
nathannolacio <nathannolacio04@gmail.com>
2026-05-09 15:27:09 -0300

    :recycle: refactor: Added anchors targets for navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,26 @@ export default function Home() {
       <Container>
         <NavBar />
       </Container>
+      <section id="home">
+        <Container>
+          <h2>Home</h2>
+        </Container>
+      </section>
+      <section id="about">
+        <Container>
+          <h2>About</h2>
+        </Container>
+      </section>
+      <section id="projects">
+        <Container>
+          <h2>Projects</h2>
+        </Container>
+      </section>
+      <section id="contact">
+        <Container>
+          <h2>Contact</h2>
+        </Container>
+      </section>
     </main>
   );
 }
